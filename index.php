eb3c27ae740cb682d62813bcb50b7f84d2d1cf2f
Jeremy Mullis <jeremy.mullis@gmail.com>
2016-11-01 16:07:56 -0500

    add CSV parsing, robots.txt

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
-		$dataJSON = file_get_contents("data.json");
-		$data = json_decode($dataJSON, true);
+	if (file_exists('data.csv')) {
+		$rows = array_map('str_getcsv', file('data.csv'));
+		$header = array_shift($rows);
+		$data = array();
+		foreach ($rows as $row) {
+		  $data[] = array_combine($header, $row);
+		}	
+	}
+	elseif (file_exists('data.json')) {
+			$dataJSON = file_get_contents("data.json");
+			$data = json_decode($dataJSON, true);
+	}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>My Company Signatures</title>
+	<title>Example E-mail Signatures</title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<script   src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.15/clipboard.min.js"></script>
@@ -23,16 +33,24 @@
 <body>
 	<div class="container">
 		<div class="jumbotron">
-			<h1>My Company E-mail Signatures</h1>
+			<h2>My Company E-mail Signatures</h2>
 			<p>Scroll down the page to find your name and click the button marked "Copy Signature".</p>
 		</div>
-		<?php
-			$index = 1;
-			foreach ($data[people] as $person) {
-				include('signature.php');
-				$index++;
-			}
-		?>
+		<?php foreach ($data as $key => $person) : ?>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><?= ($person[lastName] ? $person[lastName] : ''); ?>, <?= ($person[firstName] ? $person[firstName] : '') ?></h3>
+			</div>
+			<div class="panel-body">
+				<div id="person-<?= $key ?>">
+				<?php include('signature.php'); ?>
+				</div>
+			</div>
+			<div class="panel-footer">
+				<button class="btn btn-default" data-my-clipboard data-clipboard-target="#person-<?= $key ?>" data-clipboard-success="{{ person.firstName }} {{ person.middleName }} {{ person.lastName }}"><span class="glyphicon glyphicon-copy"></span> Copy Signature</button>
+			</div>
+		</div>
+		<?php	endforeach;	?>
 </div>
 
 </body>
